f1d9495fe7d0b598c5da5b171e0713e93fce79c2
SeanOpiso <atarusrfl@gmail.com>
2026-03-10 13:15:04 +0800

    balls

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_db_connection():
     conn = sqlite3.connect(DB_FILE)
     conn.row_factory = sqlite3.Row
     return conn
-#ballsacks
+#ballsacks yes
 @app.route('/')
 def index():
     return render_template('index.html')
